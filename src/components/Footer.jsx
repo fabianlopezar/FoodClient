619b60c7e1ee2a23c9711e5db8c5da39d0947307
FABIAN ESTEBAN LOPEZ ARIAS <fabian_esteban.lopez@uao.edu.co>
2024-11-19 19:10:41 -0500

    [Modify] [FL] Modificacion Landing.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,9 @@ function Footer() {
             <h4>Creador:</h4>
             <ul className="list-unstyled">
               <li>Fabian Esteban Lopez Arias</li>
-              <li>fabianlopez928@gmail.com</li>
+              <li>fabian_lopez.edu.co</li>
+              <li>David Roa</li>
+              <li>david@uao.edu.co</li>
             </ul>
           </div>
             {/* Column2 */}
