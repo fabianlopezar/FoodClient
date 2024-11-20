43d82d7113e8c2417017ca660d47cd5c76541507
DAVID SANTIAGO ROA MAYOR <david.roa@uao.edu.co>
2024-11-19 20:46:08 -0500

    [Modify] [DR] Se le agrego el responsive al landing.

--- src/vistas/Landing.jsx
+++ src/vistas/Landing.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from "../styles/Landing.module.css";
 import Footer from "../components/Footer";
-import video from "../img/trailer.mp4"
+import video from "../img/trailer.mp4";
 
 export default function Landing() {
   return (
-    <div>
+    <div className={style.fondo}>
       <video src={video} autoPlay muted loop className={style.video}></video>
-      <div className={style.div} >
+      <div className={style.div}>
         <h1 className={style.title}>¡ 🥣Welcome🥣 !</h1>
         <h1 className={style.subTitle}>🍳 Proyecto 🥗 Final 🥗 UAO 🍳</h1>
         <Link to="/home">
           <button className={style.botonLanding}>Enter</button>
         </Link>
-        <Footer/>
-        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
